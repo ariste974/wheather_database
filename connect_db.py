025355b4256b05a6c42571fc2df0c42e5f26addc
ariste974 <aristemathiot@gmail.com>
2025-12-14 16:01:35 +0100

    database

---
+++ connect_db.py
@@ -0,0 +1,39 @@
+import psycopg2
+
+def insert_data(row):
+    conn = psycopg2.connect(
+        host="16.170.202.253",
+        port=5432,
+        dbname="weatherdb",
+        user="weatheruser",
+        password="27EASWeatherpass!"
+    )
+
+    cursor = conn.cursor()
+
+    query = """
+    INSERT INTO meteo (
+        time,
+        temperature_2m,
+        sunshine_duration_s,
+        prcp,
+        wind_kph,
+        wind_degree,
+        wind_dir
+    )
+    VALUES (%s, %s, %s, %s, %s, %s, %s);
+    """
+
+    cursor.execute(query, (
+        row["timestamp_utc"],                           # datetime → OK
+        float(row["temperature_2m"]) if row["temperature_2m"] is not None else None,
+        float(row["sunshine_duration_s"]) if row["sunshine_duration_s"] is not None else None,
+        float(row["prcp"]) if row["prcp"] is not None else None,
+        float(row["wind_kph"]) if row["wind_kph"] is not None else None,
+        int(row["wind_degree"]) if row["wind_degree"] is not None else None,
+        str(row["wind_dir"]) if row["wind_dir"] is not None else None
+    ))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 import pandas as pd
 import requests
-from meteostat import Hourly
+from meteostat import Hourly, Stations
 import openmeteo_requests
 import requests_cache
 from retry_requests import retry
@@ -10,6 +10,8 @@ from meteostat_api import get_meteostat_current
 from openmeteo_api import get_openmeteo_current
 from wheather_api import get_weatherapi_current
 
+from connect_db import insert_data
+
 from dotenv import load_dotenv
 import os
 
@@ -17,17 +19,35 @@ load_dotenv()
 # --------------------------------------------
 # PARAMÈTRES
 # --------------------------------------------
-LAT = 52.52
-LON = 13.41
-STATION = "72219"      # Meteostat station
-CITY = "Berlin"
+# Coordonnées et ville pour Riga
+
+
+
+list_ville=[
+  {"city":"Riga","country":"Latvia","lat":56.9496,"lon":24.1052, "station": None},
+  {"city":"Paris","country":"France","lat":48.8566,"lon":2.3522, "station": None},
+  {"city":"Berlin","country":"Germany","lat":52.52,"lon":13.405, "station": None},
+  {"city":"London","country":"UK","lat":51.5074,"lon":-0.1278, "station": None}
+]
+for ville in list_ville:
+    CITY=ville["city"]
+    LAT=ville["lat"]
+    LON=ville["lon"]
+    # Recherche de la station Meteostat la plus proche automatiquement
+    stations = Stations()
+    near = stations.nearby(LAT, LON).fetch(1)
+
+    if not near.empty:
+        # L'identifiant de station est l'index du DataFrame retourné
+        ville["station"] = str(near.index[0])
+    
 WEATHERAPI_KEY = os.getenv("API_KEY")
 
 
 # ===============================================================
 # PIPELINE 
 # ===============================================================
-def get_current_weather():
+def get_current_weather(LaTT, LON, CITY, STATION):
     print("Fetching Open-Meteo...")
     open_data = get_openmeteo_current(LAT, LON)
 
@@ -42,13 +62,24 @@ def get_current_weather():
     all_data["timestamp_utc"] = datetime.utcnow()
 
     df = pd.DataFrame([all_data])
-    return df
+    row=df.iloc[0]
+    print("Data fetched successfully:", df)
+
+    insert_data(row)
+    
+    return "Data inserted into the database."
 
 
 # ===============================================================
 # MAIN
 # ===============================================================
 if __name__ == "__main__":
-    df = get_current_weather()
-    print("\n===== Données météo de maintenant =====")
-    print(df)
+    for ville in list_ville:
+        LAT=ville["lat"]
+        LON=ville["lon"]
+        CITY=ville["city"]
+        STATION=ville["station"]
+        print(f"\n===== Traitement pour la ville de {CITY} =====")
+        result = get_current_weather(LAT, LON, CITY, STATION)
+        print(result)
+    
